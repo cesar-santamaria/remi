5b39439d5fd881a9568d20ee33ece7a87d6e65ea
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-07-20 13:25:19 +0100

    Refactor server.js

--- server.js
+++ server.js
@@ -5,37 +5,14 @@ const axios = require('axios')
 const { getToken, getPlaylist } = require('./helpers/spotify')
 const ikea = require('ikea-name-generator')
 require('dotenv').config()
-
 const PORT = 8080
+
 
 //socket IO
 const socketio = require('socket.io')
 const http = require('http')
 const server = http.createServer(app)
 const io = socketio(server)
-
-let users = []
-
-//Socket listeners
-io.on('connection', (socket) => {
-  console.log('User has connected')
-  const name = ikea.getName()
-  socket.name = name
-  users.push(name)
-
-  socket.emit('INITIAL_CONNNECTION', { name, users })
-  socket.broadcast.emit('NEW_USER', { name })
-
-  socket.on('disconnect', () => {
-    console.log('User has connected ', socket.handshake.query.username)
-    users.push(socket.handshake.query.username)
-    console.log('users: ', users)
-  })
-})
-
-socket.on('Guess', guess => {
-  socket.to(roomId).emit('chat-messages', `${user}: ${guess}`)
-})
 
 // Express Configuration
 app.use(BodyParser.urlencoded({ extended: false }))
@@ -44,6 +21,7 @@ app.use(express.static('public'))
 
 let token = ''
 getToken().then((res) => (token = res.data.access_token))
+
 
 // Sample GET route
 app.get('/api/data', (req, res) => {
@@ -55,4 +33,23 @@ app.get('/api/data', (req, res) => {
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`seems to be listening on port ${PORT} 🙉`)
+})
+
+let users = []
+
+//Socket listeners
+io.on('connection', (socket) => {
+  const user = socket.handshake.query.username;
+  const roomId = socket.handshake.query.roomId;
+  console.log('User has connected', user )
+  users.push(user)
+  console.log("users: ", users)
+  socket.join(roomId);
+
+  socket.emit('INITIAL_CONNNECTION', { name, users })
+  socket.broadcast.emit('NEW_USER', { name })
+
+  socket.on('Guess', guess => {
+    socket.to(roomId).emit('chat-messages', `${user}: ${guess}`)
+  })
 })
