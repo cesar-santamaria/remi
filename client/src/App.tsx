5e0349bd2a0be7921cf4746cfef8ad46d5d4b566
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-07-20 13:25:19 +0100

    Change App to TS

---
+++ client/src/App.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import './App.css'
+
+import AudioPlayer from './AudioPlayer'
+import UserForm from './components/UserForm'
+import Game from './Game'
+import { getRoomId } from "./util/roomGenerator";
+import { IUser, ISocket } from "./interfaces/AppInterfaces";
+
+//Socket io client
+import socketIOClient from 'socket.io-client'
+const ENDPOINT = '/'
+
+const App = () => {
+  // Grab the window URL and set the Room ID to that url. URL should be formatted as localhost:3000/?[:roomId]
+  const roomId: string = getRoomId();
+
+  const [user, setUser] = useState({
+    username: '',
+    roomId: roomId,
+    score: 0,
+  })
+
+  const [socket, setSocket] = useState<ISocket | undefined>(undefined)
+
+  const fetchData = () => {
+    axios.get("/api/data").then((response) => {
+      // handle success
+      console.log(response.data);
+    });
+  }
+  const createSocket = (username: string): void => {
+    setUser(prev => {
+      return { 
+        ...prev,
+        username: username,
+      }
+    });
+    setSocket(socketIOClient(ENDPOINT, {
+      query: { username: username, roomId: user.roomId }
+    }));
+  };
+
+  return (
+    <div className="App">
+      <h1>Fetch tracks and print to console</h1>
+      <button onClick={fetchData}>Fetch Music Data</button>
+
+      {user.username ? (
+        <Game user={user} socket={socket} />
+      ) : (
+        <UserForm createSocket={createSocket} />
+      )}
+    </div>
+  )
+}
+
+export default App
