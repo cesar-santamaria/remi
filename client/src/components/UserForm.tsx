2953d3680eb55cd49220d6c7615c115b64d7bb31
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-07-23 13:25:19 +0100

    Change component to TS

---
+++ client/src/components/UserForm.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { IUserFormProps } from '../interfaces/UserFormInterfaces';
+
+
+const UserForm = (props: IUserFormProps) => {  
+  const[username, setUsername] = useState('');
+
+  const userNameSubmitted = (e: React.FormEvent<HTMLFormElement>): void => {
+      e.preventDefault();
+      if (username === '') {
+        return;
+      }
+      props.createSocket(username);
+  }
+
+  return (
+    <div id="user-form">
+      <form onSubmit = {userNameSubmitted}>
+        <input 
+          type='text'
+          id='username' 
+          placeholder='Enter Username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}/>
+        <button type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default UserForm
