6362a913773b160e19f0fd2899221d5b31d915e5
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-07-27 17:25:19 +0100

    Add find room index/user index

--- helpers/game.ts
+++ helpers/game.ts
@@ -13,3 +13,22 @@ const getTrack = (rooms: Irooms[], roomId: string): Itracks => {
 
   return rooms[currentRoom].currentTrack;
 };
+
+const findRoomIndex = (rooms: Irooms[], roomId: string): number | undefined => {
+  if (!rooms) {
+    return;
+  }
+
+  return rooms.findIndex(({ id }) => id === roomId);
+};
+
+const findUserIndex = (room: Irooms, idToFind: string): number | undefined => {
+  let retIndex = -1;
+  if (!room) return;
+  if (room.users === [] || !room.users) return;
+  retIndex = room.users.findIndex(({ id }) => id === idToFind);
+
+  return retIndex;
+};
+
+module.exports = { getTrack, findRoomIndex, findUserIndex };
