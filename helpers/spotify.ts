fe4acba2037c17cc3be37761c9558fe8081a6d83
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-07-28 17:25:19 +0100

    Fix axios start game request

--- helpers/spotify.ts
+++ helpers/spotify.ts
@@ -1,8 +1,8 @@
-import { Axios, AxiosResponse } from 'axios'
-import { Itracks, IArtist, Isonglist } from '../interface'
+import { Axios, AxiosResponse } from "axios";
+import { Itracks, IArtist, Isonglist } from "../interface";
 
-const qs = require('qs')
-const axios = require('axios')
+const qs = require("qs");
+const axios: Axios = require("axios");
 
 /* Obtain the spotify access token to access playlists and track previews.
  * To obtain spotify access keys, follow steps taken in https://developer.spotify.com/documentation/general/guides/authorization/
@@ -10,25 +10,26 @@ const axios = require('axios')
  *
  * @return - axios promise containing the access token which will expire in 1 hour.
  */
-const getToken = () => {
-  const client_id = process.env.CLIENT_ID
-  const client_secret = process.env.CLIENT_SECRET
-  const auth_token = Buffer.from(
-    `${client_id}:${client_secret}`,
-    'utf-8'
-  ).toString('base64')
+const getToken = (): Promise<void | AxiosResponse<any, any>> => {
+  const clientId = process.env.CLIENT_ID;
+  const clientSecret = process.env.CLIENT_SECRET;
+  const authToken = Buffer.from(
+    `${clientId}:${clientSecret}`,
+    "utf-8"
+  ).toString("base64");
 
-  const url = 'https://accounts.spotify.com/api/token'
+  const url = "https://accounts.spotify.com/api/token";
   const headers = {
-    Authorization: 'Basic ' + auth_token,
-    'Content-Type': 'application/x-www-form-urlencoded',
-  }
-  const body = qs.stringify({ grant_type: 'client_credentials' })
+    Authorization: "Basic " + authToken,
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+  // eslint-disable-next-line camelcase
+  const body = qs.stringify({ grant_type: "client_credentials" });
 
-  return axios.post(url, body, { headers: headers }).catch((error: any) => {
-    console.log('error', error.message)
-  })
-}
+  return axios.post(url, body, { headers: headers }).catch((error) => {
+    console.log("error", error.message);
+  });
+};
 
 /* Query spotify API to obtain a playlist for song previews.
  * @params - {token} - Access token used to perform any search queries
@@ -37,30 +38,25 @@ const getToken = () => {
  *
  * @return - axios promise containing an array of tracks with all information.
  */
-const getPlaylist = (
-  token: string,
-  genre: string,
-  artist: string
-): Promise<void | AxiosResponse<any, any>> => {
-  const limit = 50 // needs to be set as a function argument
-  let apiPlaylistUrl = 'https://api.spotify.com/v1/recommendations?market=CA'
-  apiPlaylistUrl += `&limit=${limit}`
-  if (artist !== '') {
-    apiPlaylistUrl = `https://api.spotify.com/v1/artists/${artist}/top-tracks?market=CA`
+const getPlaylist = (token: string, genre: string, artist: string): Promise<void | AxiosResponse<any, any>> => {
+  const limit = 50; // needs to be set as a function argument, number of rounds?
+  let apiPlaylistUrl = "https://api.spotify.com/v1/recommendations?market=CA";
+  apiPlaylistUrl += `&limit=${limit}`;
+  apiPlaylistUrl += `&seed_genres=${genre}`;
+  if (artist !== "") {
+    apiPlaylistUrl = `https://api.spotify.com/v1/artists/${artist}/top-tracks?market=CA`;
   }
-  {
-    genre
-  }
+
   const header = {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
+    Accept: "application/json",
+    "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
-  }
-  return axios.get(apiPlaylistUrl, { headers: header }).catch((error: any) => {
-    console.log('error', error.message)
-    getToken()
-  })
-}
+  };
+  return axios.get(apiPlaylistUrl, { headers: header }).catch((error) => {
+    console.log("error", error.message);
+    getToken();
+  });
+};
 
 /* Takes a tracks array and filters out all information except the artist name and track name.
  * @params - {tracks} - takes in a successful axios.get response.
@@ -70,11 +66,11 @@ const getPlaylist = (
 const filterTitles = (tracks: Itracks[]): Isonglist[] => {
   return tracks.map((track) => {
     return {
-      artist: track.artists.map((artist: IArtist) => artist.name).join(', '),
+      artist: track.artists.map((artist: IArtist) => artist.name).join(", "),
       name: track.name,
-    }
-  })
-}
+    };
+  });
+};
 
 /* Creates a list of songs for the autocomplete function of the game.
  * @params - [{sampleSonglist}] - takes in a sample list of songs to seed it.
@@ -82,16 +78,13 @@ const filterTitles = (tracks: Itracks[]): Isonglist[] => {
  *
  * @return - {output} [{ artist, title }]
  */
-const createAutocomplete = (
-  sampleSonglist: Isonglist[],
-  titles: Isonglist[]
-): Isonglist[] => {
-  const autocomplete = [...sampleSonglist, ...titles]
+const createAutocomplete = (sampleSonglist: Isonglist[], titles: Isonglist[]): Isonglist[] => {
+  const autocomplete = [...sampleSonglist, ...titles];
   const output = [...new Set(autocomplete.map((e) => JSON.stringify(e)))].map(
     (e) => JSON.parse(e)
-  )
-  return output
-}
+  );
+  return output;
+};
 
 /* Query spotify API to obtain an array of artists and IDs for additional searching
  * @params - {token} - Access token used to perform any search queries
@@ -99,18 +92,15 @@ const createAutocomplete = (
  *
  * @return - axios promise containing an array of artists with all information provided by spotify
  */
-const queryArtist = (
-  token: string,
-  searchParam: string
-): Promise<AxiosResponse<any, any>> => {
-  const apiArtistUrl = `https://api.spotify.com/v1/search?q=${searchParam}&type=artist&market=CA&limit=5`
+const queryArtist = (token: string, searchParam: string): Promise<AxiosResponse<any, any>> => {
+  const apiArtistUrl = `https://api.spotify.com/v1/search?q=${searchParam}&type=artist&market=CA&limit=5`;
   const header = {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
+    Accept: "application/json",
+    "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
-  }
-  return axios.get(apiArtistUrl, { headers: header })
-}
+  };
+  return axios.get(apiArtistUrl, { headers: header });
+};
 
 module.exports = {
   getToken,
@@ -118,4 +108,4 @@ module.exports = {
   filterTitles,
   createAutocomplete,
   queryArtist,
-}
+};
